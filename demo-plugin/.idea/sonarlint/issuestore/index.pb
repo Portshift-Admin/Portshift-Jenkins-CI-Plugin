e058f3bdee53085d71424291e05e638313d747da
rafi <rafi@portshift.io>
2020-01-26 17:13:52 +0200

    first draft

---
+++ demo-plugin/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+
+Qsrc/main/java/io/jenkins/plugins/sample/PortshiftVulnerabilityScannerBuilder.java,0/c/0c3c25e450f54f00e9cdffe326bc00784d196be2
---
+++ demo-plugin/src/main/java/io/jenkins/plugins/sample/PortshiftScannerAction.java
@@ -0,0 +1,60 @@
+package io.jenkins.plugins.sample;
+
+
+import hudson.model.Action;
+import hudson.model.Run;
+
+public class PortshiftScannerAction implements Action {
+
+  private String resultsUrl;
+  private Run<?, ?> build;
+  private String artifactSuffix;
+  private String title;
+
+  public PortshiftScannerAction(Run<?, ?> build, String artifactSuffix, String artifactName, String title) {
+    this.build = build;
+    this.artifactSuffix = artifactSuffix;
+    this.resultsUrl = "../artifact/" + artifactName;
+    this.title = title;
+  }
+
+  @Override
+  public String getIconFileName() {
+    // return the path to the icon file
+    return "/plugin/aqua-microscanner/images/MicroScanner.png";
+  }
+
+  @Override
+  public String getDisplayName() {
+    // return the label for your link
+    if (title != null) {
+      return title;
+    }
+    if (artifactSuffix == null) {
+      return "Aqua MicroScanner";
+    } else {
+      return "Aqua MicroScanner " + artifactSuffix;
+    }
+  }
+
+  @Override
+  public String getUrlName() {
+    // defines the suburl, which is appended to ...jenkins/job/jobname
+    if (artifactSuffix == null) {
+      return "aqua-results";
+    } else {
+      return "aqua-results-" + artifactSuffix;
+    }
+  }
+
+  public Run<?, ?> getBuild() {
+    return this.build;
+  }
+
+  public String getResultsUrl() {
+    return this.resultsUrl;
+  }
+
+
+}
+
